55ea598e1c9901552b8aaffd47796be6efe645bc
lawrencebalu15-art <lawrencebalu15@gmail.com>
2026-08-30 18:58:31 +0530

    udate

--- dist/assets/support-D-aq9v7L.js
+++
@@ -1,1 +0,0 @@
-import"./supabase-C4TjTvDq.js";import"./main-Dw1_MU5N.js";document.addEventListener("DOMContentLoaded",()=>{console.log("Support page loaded successfully.")});
---
+++ dist/assets/support-D5KSlD6B.js
@@ -0,0 +1,1 @@
+import"./supabase-C4TjTvDq.js";import"./main-Bv_W86yG.js";document.addEventListener("DOMContentLoaded",()=>{console.log("Support page loaded successfully.")});
